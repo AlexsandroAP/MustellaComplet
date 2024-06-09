ac7ff8d4e86cc1a59bc9808041f14610b9244e60
AlexsandroAP <alexpotteralma@gmail.com>
2024-06-09 13:22:22 -0300

    kll

--- resources/views/favorites-index.blade.php
+++ resources/views/favorites-index.blade.php
@@ -25,18 +25,25 @@
             <x-addPost></x-addPost>
 
             
-            <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-                <div class="h-16">
-                    <div class="flex md:flex-row justify-between">
+            <nav x-data="{ open: false }" class="pt-10 pb-5 bg-high-purplle border-none fixed top-0 left-0 w-full z-50border-white">
+                <!-- Primary Navigation Menu -->
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div class="flex justify-between h-16">
+                        <div class="flex">
 
 
-                        <!-- Navigation Links -->
-                        <div class="flex justify-center items-center space-x-8 sm:-my-px sm:ms-10 sm:flex text-white">
-                            <x-nav-link :href="route('mustella')" :active="request()->routeIs('mustella')">
+                            <!-- Navigation Links -->
+                            <div class="flex justify-center items-center space-x-8 sm:-my-px sm:ms-10 sm:flex text-white">
+
+
+                               <a href="{{ url()->previous() }}">
+
                                 <img class="w-10 h-10 min-w-10 min-h-10" src="{{ asset('imagens/seta.png') }}" alt="seta">
-                            </x-nav-link>
+                               </a>
+                            </div>
                         </div>
 
+                        <!-- Settings Dropdown -->
                         <div class="hidden sm:flex sm:items-center sm:ms-6 rounded-full my-9 text-black">
                             <x-dropdown align="right" width="48">
                                 <x-slot name="trigger">
@@ -81,45 +88,45 @@
                         </div>
                     </div>
                 </div>
-            
+
                 <!-- Responsive Navigation Menu -->
                 <div :class="{'block': open, 'hidden': ! open}" class="hidden text-white sm:hidden">
-            
+
                     <div class="pt-2 pb-3 space-y-1">
                         <x-responsive-nav-link :href="route('mustella')" :active="request()->routeIs('mustella')">
                             {{ __('mustella') }}
                         </x-responsive-nav-link>
                     </div>
-            
+
                     <!-- Responsive Settings Options -->
                     <div class="pt-1 pb-1 border-none border-gray-200">
                         <div class="px-4">
                             <div class="font-medium text-base text-gray-800">{{ Auth::user()->name }}</div>
                             <div class="font-medium text-sm text-gray-500">{{ Auth::user()->email }}</div>
                         </div>
-            
+
                         <div class="space-y-1">
                             <x-responsive-nav-link :href="route('perfil')">
                             {{ __('Perfil') }}
                             </x-responsive-nav-link>
                         </div>
-            
+
                         <div class="mt-1 space-y-1">
                             <x-responsive-nav-link :href="route('profile.edit')">
                                 {{ __('Configurações') }}
                             </x-responsive-nav-link>
                         </div>
-            
+
                         <div class="mt-1 mb-1 space-y-1">
                             <x-responsive-nav-link :href="route('mustella')">
                                 {{ __('Tela Inicial') }}
                             </x-responsive-nav-link>
                         </div>
-            
+
                             <!-- Authentication -->
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
-            
+
                                 <x-responsive-nav-link :href="route('logout')"
                                         onclick="event.preventDefault();
                                                     this.closest('form').submit();">
@@ -129,13 +136,10 @@
                         </div>
                     </div>
                 </div>
-
-                    </div>
-                </div>
-            </div>
+            </nav>
 
             <div class="md:px-20rem px-5 md:mt-3rem mt-7rem grid md:grid-cols-3 gap-4">
-                        
+
 
                 @if ($favorites->count() > 0)
                         @foreach($posts as $post)
@@ -152,16 +156,16 @@
 
 
                                         <div class="text-purplle flex-grow">
-                                           
+
                                             <x-dropdown align="left" width="48">
                                                 <x-slot name="trigger">
                                                     <button>
                                                         <img class="w-5 h-5 m-4 min-w-5 min-h-5" src="{{ asset('imagens/points.png') }}" alt="seta">
                                                     </button>
                                                 </x-slot>
-                            
+
                                                 <x-slot name="content">
-                            
+
                                                     <x-dropdown-link >
                                                         <form action="{{ route('posts.destroy', $post->id) }}" method="post">
                                                             @csrf
@@ -174,30 +178,26 @@
                                                         <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary">Editar</a>
 
                                                     </x-dropdown-link>
-                            
-                                                    
+
+
                                                 </x-slot>
-                                               
+
                                             </x-dropdown>
 
 
                                         </div>
 
                                         </div>
-                                       
+
                                     </div>
 
                                     <div class="font-bold text-xl mb-2 text-white">
                                         <a href="{{ route('posts.show', $post) }}">{{ $post->title }}</a>
                                     </div>
-                                    
+
                                     <div class="flex justify-between">
                                         <p class="text-base php artisan storage:link text-orange">{{ $post->caption }}</p>
-                                        
-                            
                                     </div>
-
-
 
                                 </div>
 
@@ -207,13 +207,13 @@
 
                         {{ $favorites->links() }}
                         @else
-                            <p>Você não possui posts favoritados.</p>
+                            <p class="text-white text-center">Você não possui posts favoritados.</p>
                         @endif
-                        
-                    </div>
-                
-            </main>
+
+            </div>
+
+        </main>
             
-        </div>
-    </body>
+    </div>
+</body>
 </html>
